f27082cfeaaaffb24efa181061cf8e0d41fbc570
ANTU SHIL <anturoy084@gmail.com>
2025-12-11 23:44:49 +0600

    add stripe

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
 app.use(cors());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6si5fpl.mongodb.net/?appName=Cluster0`;
 
@@ -21,31 +20,75 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
+    const db = client.db("asset_verse_db");
+    const parcelsCollection = db.collection("parcels");
 
-    const db = client.db('asset_verse_db')
-    const parcelsCollection = db.collection('parcels')
+    //parcel api
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      // const {email} = req.query;
 
-    app.post('/parcels', async(req, res) =>{
-        const parcel = req.body;
-        const result = await parcelsCollection.insertOne(parcel)
-        res.send(result)
-    })
+      // if(email){
+      //   query.hrEmail = email;
+      // }
 
+      const options = { sort: { createdAt: -1 } };
 
+      const cursor = parcelsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      parcel.createdAt = new Date();
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-    
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // payment related api
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data:{
+              currency:'USD',
+              unit_amount:1500,
+              product_data:{
+                name: paymentInfo.parcelName
+              }
+               
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.hrEmail,
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/HR-Manager/Upgrade-Package-success`,
+      });
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,13 +96,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
